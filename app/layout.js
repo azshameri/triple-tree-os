70b09d36cae8aaad5ebda00b26a53e1056ce2dc1
azshameri <42930225+azshameri@users.noreply.github.com>
2026-04-05 17:17:15 +0600

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "Triple Tree OS",
+  description: "Minimum counts. Maximum grows."
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body style={{ margin: 0 }}>{children}</body>
+    </html>
+  );
+}
